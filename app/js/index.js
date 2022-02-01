de1ea3e0e30122d0622c2216508fb683e7d0f8a8
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-01 14:15:45 -0300

    added aroma page, working on mobile

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,5 @@
 function toggleMenu () {
+   const BODY = document.querySelector('body');
    const MENU_BUTTON = document.getElementById('menuButton');
    const MENU = document.getElementById('menu');
    const MENU_CLOSED = document.getElementById('menuClosed');
@@ -6,6 +7,7 @@ function toggleMenu () {
    const SHARE = document.getElementById('share');
 
     MENU_BUTTON.addEventListener('click', () => {
+        BODY.classList.add('no-scroll');
         MENU_BUTTON.classList.add('hidden');
         MENU_CLOSED.classList.remove('hidden');
         MENU.classList.remove('hidden')
@@ -13,6 +15,7 @@ function toggleMenu () {
     })
    
     SHARE_BUTTON.addEventListener('click', () => {
+       BODY.classList.add('no-scroll')
        SHARE_BUTTON.classList.add('hidden');
        MENU_BUTTON.classList.remove('show');
        MENU_BUTTON.classList.add('hidden');
@@ -22,6 +25,7 @@ function toggleMenu () {
     })
 
     MENU_CLOSED.addEventListener('click', () => {
+        BODY.classList.remove('no-scroll');
         MENU_BUTTON.classList.remove('hidden');
         SHARE_BUTTON.classList.remove('hidden');
         MENU_CLOSED.classList.add('hidden');
